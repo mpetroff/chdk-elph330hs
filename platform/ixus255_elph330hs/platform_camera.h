f8ca06bd03e2ee3bca59ab36b93cfe1606ce1410
Matthew Petroff <matthew@mpetroff.net>
2014-03-01 15:27:52 -0500

    Minor changes to platform_camera.h.

--- platform/ixus255_elph330hs/platform_camera.h
+++ platform/ixus255_elph330hs/platform_camera.h
@@ -65,7 +65,6 @@
 
     // long shutter is actually user TV, may work ?
     #undef  CAM_HAS_USER_TV_MODES
-    #define CAM_SHOW_OSD_IN_SHOOT_MENU      1
 
     #define  CAM_EXT_TV_RANGE               1    // CHDK can make exposure time longer than 64s
 
@@ -108,8 +107,6 @@
                                                 // see http://chdk.setepontos.com/index.php/topic,3994.405.html
 
     #define CAM_ADJUSTABLE_ALT_BUTTON                   1
-//    #undef CAM_DEFAULT_ALT_BUTTON
-//    #define CAM_DEFAULT_ALT_BUTTON  KEY_PLAYBACK   // no print button
 
     #define CAM_ALT_BUTTON_NAMES                        { "Playback", "Video" }
     #define CAM_ALT_BUTTON_OPTIONS                      { KEY_PLAYBACK, KEY_VIDEO }
@@ -128,7 +125,7 @@
     #undef CAM_CAN_SD_OVER_NOT_IN_MF
     #define  CAM_CAN_SD_OVER_IN_AF_LOCK_ONLY     1 // Camera allows subject distance (focus) override only when in AF Lock mode OR in movie mode
 
-    // TODO
+    // Seems to work
     #define CAM_DATE_FOLDER_NAMING                  0x400
 
     #define PARAM_CAMERA_NAME               3       // parameter number for GetParameterData
